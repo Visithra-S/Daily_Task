1b5be8e9cef68281f9b7879f60b5da82d426b12d
Visithra-S <visithrasubramanian@gmail.com>
2023-09-28 21:50:14 +0530

    initial commit

---
+++ StringExample.java
@@ -0,0 +1,10 @@
+class StringExample{
+	public static void main(String args[]){
+		String var = new String("visi"); //String first way of object creation using new keyword.
+		String var1 = "visithrasubramanian"; //Second way of object creation using string literal.
+		var1 = "vs";
+		
+		System.out.println(var);
+		System.out.println(var1);
+	}
+}
