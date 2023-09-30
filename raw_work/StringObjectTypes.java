3db0f7c37fa88ce3e7612c9f490388ce90f753ff
Goutham <ggowthamsankar@gmail.com>
2023-09-30 18:12:44 +0530

    Update new dir

---
+++ raw_work/StringObjectTypes.java
@@ -0,0 +1,10 @@
+class StringObjectTypes{
+	public static void main(String args[]){
+	
+		String str = new String("Visithra S");
+		System.out.println("First Type of Object creation using new keyword : " + str);
+		
+		String str1 = "Visi";
+		System.out.println("Second type of Object Creation using string literals : " + str1);	
+	}
+}
