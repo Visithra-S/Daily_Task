6e755f137907f24f98f1dcb3fb7183b1e350619f
Goutham <ggowthamsankar@gmail.com>
2023-09-30 18:01:26 +0530

    Update .java & .class

--- Day_3.java
+++ Day_3.java
@@ -4,9 +4,19 @@ class Day_3{
 
 	public static void main(String[] gmsk){
 		
-		String separator = ",";
+		final String separator = ","; 
 		Arrays.asList("a", "b", "c").forEach(e -> System.out.println(e));
 		
 		Arrays.asList("v", "i", "s", "i").forEach((String e) -> System.out.println(e + separator));
+
+		// The return statement is not required if the lambda body is just a one-liner.
+
+//		int output = Arrays.asList( "a", "b", "d" ).sort( ( e1, e2 ) -> {
+//				int result = e1.compareTo( e2 );
+//				return result;
+//			} );
+
+//		System.out.println("output = "+output);
+
 	}
 }
