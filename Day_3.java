1b686b1df0d2592dbc547e9543b87c20e9c249dd
Goutham <ggowthamsankar@gmail.com>
2023-09-30 17:31:20 +0530

    initial commit

---
+++ Day_3.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Day_3{
+
+	public static void main(String[] gmsk){
+		
+		String separator = ",";
+		Arrays.asList("a", "b", "c").forEach(e -> System.out.println(e));
+		
+		Arrays.asList("v", "i", "s", "i").forEach((String e) -> System.out.println(e + separator));
+	}
+}
