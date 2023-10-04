c7849bb005d132aae9cf958594ac11ca109dbca0
Visithra-S <visithrasubramanian@gmail.com>
2023-10-04 16:01:20 +0530

    changes in methodDemo.java

--- raw_work/MethodDemo.java
+++ raw_work/MethodDemo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class MethodDemo{
 	
 	static String personelDetails(String firstName,char lastName){
-		return firstName + "" +lastName;
+		return firstName + " " +lastName;
 	}
 	public static void main(String args[]){
 		
@@ -11,12 +11,14 @@ class MethodDemo{
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Enter your FirstName : ");
-		String firstname = scanner.nextLine();
-		System.out.println("Enter your LastName : ");
-		char lastname = scanner.next().charAt(0);
-		String fullName = personelDetails(firstname, lastname);
-		//personelDetails("Visithra" , "S");
-		System.out.println("My fullName is  " + fullName);
+			System.out.println("Enter your FirstName : ");
+			String firstname = scanner.nextLine();
+			System.out.println("Enter your LastName : ");
+			char lastname = scanner.next().charAt(0);
+			String fullName = personelDetails(firstname, lastname);
+			
+			for(int i = 0; i < name.length; i++){
+				System.out.println("My fullName is  " + fullName);	
+			}
 	}
 }
