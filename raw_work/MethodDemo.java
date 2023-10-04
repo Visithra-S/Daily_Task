40b83a2748319ca2dd5b5015233e1de489ee6ccb
Goutham <ggowthamsankar@gmail.com>
2023-10-04 18:29:26 +0530

    Update MethodDemo.java

--- raw_work/MethodDemo.java
+++ raw_work/MethodDemo.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 class MethodDemo{
 	
 	static String personelDetails(String firstName,String lastName){
-		return firstName + " " +lastName;
+		if(firstName.length() == 0 && lastName.length() == 0){
+		 return "Does`t Provide the First Name and Last Name.";
+		}
+		 return firstName + " " +lastName;
+		
 	}
 	
 	public static void main(String args[]){
-		
+
+		String firstname = null, lastname = null;	
+	
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Enter your FirstName : ");
-		String firstname = scanner.nextLine();
+		firstname = scanner.nextLine();
 		
 		System.out.println("Enter your LastName : ");
-		String lastname = scanner.nextLine();
+		lastname = scanner.nextLine();
 		
 		String fullName = personelDetails(firstname, lastname);
 		
