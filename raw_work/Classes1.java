9cd9c48327936a33f9eb206e72ab9d60d6ed071f
Visithra-S <visithrasubramanian@gmail.com>
2023-10-05 22:03:59 +0530

    new updates

---
+++ raw_work/Classes1.java
@@ -0,0 +1,12 @@
+class Classes1{
+	
+	static String name;
+	static int age;
+public class Classes2{
+	
+	public static void main(String visi[]){
+		System.out.println(Classes1.name("Visithra"));
+				System.out.println(Classes1.age(22));
+	}
+}
+}
