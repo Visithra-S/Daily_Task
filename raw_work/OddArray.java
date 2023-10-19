bb7c984c51a084d0d85569a4bc7678d18e43dc3d
Visithra-S <visithrasubramanian@gmail.com>
2023-10-19 12:46:22 +0530

    new changes

--- raw_work/OddArray.java
+++ raw_work/OddArray.java
@@ -4,7 +4,7 @@ public class OddArray{
 		 int[] number = {10, 44, 555, 999, 5, 6, 77, 50, 633, 80, 40, 550, 440, 444, 603, 708, 503, 555,  03, 05, 80, 702, 201, 308, 397};
 		 
 		 try{
-		  	for(int i = 0; i <= number.length; i++){
+		  	for(int i = 0; i < number.length; i++){
 		 		if(number[i]%2 == 0){
 		 			System.out.println(number[i] +" Even number.\n");
 		 		}else{
